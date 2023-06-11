bc79c7f14df909d1c6fd02264749bb8f99e78107
JohnKing <john.onyango@systechafrica.com>
2023-06-11 20:00:09 +0300

    commented ogin tester test to for debugging purposes

--- src/test/java/org/selenium/tests/LoginTester.java
+++ src/test/java/org/selenium/tests/LoginTester.java
@@ -12,11 +12,11 @@ import java.io.IOException;
 
 public class LoginTester extends BaseClass {
 
-    @Test
-    public void login() throws IOException, IllegalAccessException {
-//        BillingAddress billingAddress = JacksonUtils.deserialization("billingAddress.json", BillingAddress.class);
-        User user = new User(ConfigLoader.getInstance().getUsername(), ConfigLoader.getInstance().getPassword());
-        LoginPage loginPage = new LoginPage(getDriver());
-        loginPage.login(user);
-    }
+//    @Test
+//    public void login() throws IOException, IllegalAccessException {
+////        BillingAddress billingAddress = JacksonUtils.deserialization("billingAddress.json", BillingAddress.class);
+//        User user = new User(ConfigLoader.getInstance().getUsername(), ConfigLoader.getInstance().getPassword());
+//        LoginPage loginPage = new LoginPage(getDriver());
+//        loginPage.login(user);
+//    }
 }
